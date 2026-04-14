5aa2c8dec9c37513aafbaf82f652b9b34b598bef
Heli255 <helijava.aorc@gmail.com>
2026-04-14 21:34:16 +0530

    Create Book,Loan and Member Repositories

---
+++ src/main/java/com/bookvault/bookvault/repository/BookRepository.java
@@ -0,0 +1,11 @@
+package com.bookvault.bookvault.repository;
+
+import com.bookvault.bookvault.entity.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, UUID> {
+}
